3d1084f06af07cc2b22c02c91d423c2b34244eb1
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-05 10:32:26 -0600

    changed inputs back to text and updated JS to validate the text inputs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Write your JavaScript code here!
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
   let form = document.querySelector("form");
-  form.addEventListener("submit", function (event) {
+  form.addEventListener("submit", function(event) {
     let pilotName = document.querySelector("input[name=pilotName]");
     let copilotName = document.querySelector("input[name=copilotName]");
     let fuelLevel = document.querySelector("input[name=fuelLevel]");
@@ -9,12 +9,35 @@ window.addEventListener("load", function () {
     // alert("cargoMass: " + cargoMass.value);
     //alert("fuelLevel.value: " + typeof fuelLevel.value)
 
-    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-      alert('Invalid Entry! All fields are required!');
+    if (
+      pilotName.value === "" ||
+      copilotName.value === "" ||
+      fuelLevel.value === "" ||
+      cargoMass.value === ""
+    ) {
+      alert("Invalid Entry! All fields are required!");
       // stop the form submission
       event.preventDefault();
-    };
+    }
+
+    if (allnumeric(fuelLevel)) {
+      alert("fuelLevel must have numeric characters only");
+      event.preventDefault();
+    }
+
+    if (allnumeric(cargoMass)) {
+      alert("Cargo Mass must have numeric characters only");
+      event.preventDefault();
+    }
   });
+  function allnumeric(input) {
+    var numbers = /^[0-9]+$/;
+    if (input.value.match(numbers)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
                      <h2>Mission Destination</h2>
