cfd2d67d0ce6e6a0667985e0143d87746a1d2d8c
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-04 22:39:45 -0600

    added missing brackets

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function () {
-      let form = document.querySelector("form");
-      form.addEventListener("submit", function (event) {
-          let pilotName = document.querySelector("input[name=pilotName]");
-          let copilotName = document.querySelector("input[name=copilotName]");
-          let fuelLevel = document.querySelector("input[name=fuelLevel]");
-          let cargoMass = document.querySelector("input[name=cargoMass]");
-          // alert("cargoMass: " + cargoMass.value);
-          //alert("fuelLevel.value: " + typeof fuelLevel.value)
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+    // alert("cargoMass: " + cargoMass.value);
+    //alert("fuelLevel.value: " + typeof fuelLevel.value)
 
-          if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert('Invalid Entry! Input needed for all fields')
-            // stop the form submission
-            event.preventDefault();
-          });
-        // if (pilotName.value === "" || copiolotName.value === "" || fuelLevel.value ===
-        //    "" || cargoMass.value === "") {
-        //    alert("All fields are required!");
-
-
-        //       } else if (pilotName.value !== String || copiolotName.value !== String) {
-        //          alert('Pilot Names must not include numbers')
-        //       } else if (fuelLevel.value !== Number || cargomass.value !== Number) {
-        //          alert('Must not include letters')
-        //       });
-      });
-
-    /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-                         <h2>Mission Destination</h2>
-                         <ol>
-                            <li>Name: ${}</li>
-                            <li>Diameter: ${}</li>
-                            <li>Star: ${}</li>
-                            <li>Distance from Earth: ${}</li>
-                            <li>Number of Moons: ${}</li>
-                         </ol>
-                         <img src="${}">
-                         */
+    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+      alert('Invalid Entry! All fields are required!');
+      // stop the form submission
+      event.preventDefault();
+    };
+  });
+});
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+                     <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${}</li>
+                        <li>Diameter: ${}</li>
+                        <li>Star: ${}</li>
+                        <li>Distance from Earth: ${}</li>
+                        <li>Number of Moons: ${}</li>
+                     </ol>
+                     <img src="${}">
+                     */
