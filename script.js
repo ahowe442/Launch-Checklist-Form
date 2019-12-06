166e238270bd963afdd1b633e0e8be3866637efa
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-05 18:27:16 -0600

    Launch Status Check inputted and flshing in and out, needs to stay in the launch Status Check box

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
 // Write your JavaScript code here!
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   let form = document.querySelector("form");
-  form.addEventListener("submit", function(event) {
-    let pilotName = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
+  form.addEventListener("submit", function (event) {
+    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    let copilotNameInput = document.querySelector("input[name=copilotName]");
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
     // alert("cargoMass: " + cargoMass.value);
     //alert("fuelLevel.value: " + typeof fuelLevel.value)
 
     if (
-      pilotName.value === "" ||
-      copilotName.value === "" ||
-      fuelLevel.value === "" ||
-      cargoMass.value === ""
+      pilotNameInput.value === "" ||
+      copilotNameInput.value === "" ||
+      fuelLevelInput.value === "" ||
+      cargoMassInput.value === ""
     ) {
       alert("Invalid Entry! All fields are required!");
       // stop the form submission
       event.preventDefault();
     }
 
-    if (allnumeric(fuelLevel)) {
+    if (allnumeric(fuelLevelInput)) {
       alert("fuelLevel must have numeric characters only");
       event.preventDefault();
     }
 
-    if (allnumeric(cargoMass)) {
+    if (allnumeric(cargoMassInput)) {
       alert("Cargo Mass must have numeric characters only");
       event.preventDefault();
     }
+
+    let pilotStatus = document.querySelector('#pilotStatus');
+    let copilotStatus = document.querySelector('#copilotStatus');
+
+    // console.log(pilotNameInput.value);
+    // console.log(pilotStatus.innerHTML);
+    pilotStatus.innerHTML = `${pilotNameInput.value} is ready for launch`;
+    document.querySelector("#pilotStatus").style.visibility = "visible";
+
+    copilotStatus.innerHTML = `${copilotNameInput}`;
+    copilotStatus.style.visibility = "visible";
+
+    if (fuelLevelInput < 10000) {
+      document.getElementById("faultyItems").style.visibility = "visible";
+      document.getElementById('fuelStatus').innerHTML = "There is not enough fuel for the journey";
+      document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
+      document.getElementById('launchStatus').style.color = "red";
+    }
+
+    if (cargoMassInput < 10000) {
+      document.getElementById("faultyItems").style.visibility = "visible";
+      document.getElementById('fuelStatus').innerHTML = "There is too much mass for the shuttle to take off";
+    }
+
+
+
+
   });
+
   function allnumeric(input) {
     var numbers = /^[0-9]+$/;
     if (input.value.match(numbers)) {
@@ -38,15 +67,24 @@ window.addEventListener("load", function() {
       return true;
     }
   }
+
+  //Updating Shuttle Requirements
+
+
+  /* This block of code shows how to format the HTML once you fetch some planetary JSON!
+                       <h2>Mission Destination</h2>
+                       <ol>
+                          <li>Name: ${}</li>
+                          <li>Diameter: ${}</li>
+                          <li>Star: ${}</li>
+                          <li>Distance from Earth: ${}</li>
+                          <li>Number of Moons: ${}</li>
+                       </ol>
+                       <img src="${}">
+                       */
+
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-                     <h2>Mission Destination</h2>
-                     <ol>
-                        <li>Name: ${}</li>
-                        <li>Diameter: ${}</li>
-                        <li>Star: ${}</li>
-                        <li>Distance from Earth: ${}</li>
-                        <li>Number of Moons: ${}</li>
-                     </ol>
-                     <img src="${}">
-                     */
+
+
+// Ternary 
+//class='${json[index].active ? "green" : "red"'
