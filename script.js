525cc08f9a82002163cdb4475c014b70ac8eb896
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-06 20:20:14 -0600

    added mission destination[0]

--- script.js
+++ script.js
@@ -37,19 +37,25 @@ window.addEventListener("load", function () {
     };
 
 
-    //testing variables
-    // pilotNameInput = 'Abby';
-    // copilotNameInput = 'Kellie';
-    // fuelLevelInput = 3455;
-    // cargoMassInput = 2000;
-
     // call functions
-    showPilotNamesInStatusCheck();
-    checkCargoMass();
-    checkFuelLevel();
+
+    isReadyForLaunch();
 
 
+    function isReadyForLaunch() {
+      showPilotNamesInStatusCheck();
+      checkCargoMass();
+      checkFuelLevel();
 
+      if (checkCargoMass === false || checkFuelLevel === false) {
+        document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
+        document.getElementById('launchStatus').style.color = "red";
+        return false
+      } else {
+        document.getElementById('launchStatus').innerHTML = "Shuttle ready for launch";
+        document.getElementById('launchStatus').style.color = "green";
+      }
+    };
 
 
     function showPilotNamesInStatusCheck() {
@@ -64,21 +70,21 @@ window.addEventListener("load", function () {
       if (fuelLevelInput.value < 10000) {
         document.getElementById("faultyItems").style.visibility = "visible";
         document.getElementById('fuelStatus').innerHTML = "There is not enough fuel for the journey";
-        document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
-        document.getElementById('launchStatus').style.color = "red";
+        // document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
+        // document.getElementById('launchStatus').style.color = "red";
       }
+      return false;
     };
 
     function checkCargoMass() {
       if (cargoMassInput.value < 10000) {
         document.getElementById("faultyItems").style.visibility = "visible";
         document.getElementById('fuelStatus').innerHTML = "There is too much mass for the shuttle to take off";
-        document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
-        document.getElementById('launchStatus').style.color = "red";
+        // document.getElementById('launchStatus').innerHTML = "Shuttle not ready for launch";
+        // document.getElementById('launchStatus').style.color = "red";
       }
+      return false;
     };
-
-
 
   });
 
@@ -89,25 +95,25 @@ window.addEventListener("load", function () {
     } else {
       return true;
     }
-  }
+  };
 
-  //Updating Shuttle Requirements
-
-
-  /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-                       <h2>Mission Destination</h2>
+  //Mission Destination Information
+  let json = [];
+  fetch('https://handlers.education.launchcode.org/static/planets.json').then(function (response) {
+    response.json().then(function (json) {
+      const missionTarget = document.getElementById("missionTarget");
+      missionTarget.innerHTML = `
+                  <h2>Mission Destination</h2>
                        <ol>
-                          <li>Name: ${}</li>
-                          <li>Diameter: ${}</li>
-                          <li>Star: ${}</li>
-                          <li>Distance from Earth: ${}</li>
-                          <li>Number of Moons: ${}</li>
+                          <li>Name: ${json[0].name}</li>
+                          <li>Diameter: ${json[0].diameter}</li>
+                          <li>Star: ${json[0].star}</li>
+                          <li>Distance from Earth: ${json[0].distance}</li>
+                          <li>Number of Moons: ${json[0].moons}</li>
                        </ol>
-                       <img src="${}">
-                       */
+                       <img src="${json[0].image}">
+  `
+    })
+  });
 
 });
-
-
-// Ternary 
-//class='${json[index].active ? "green" : "red"'
